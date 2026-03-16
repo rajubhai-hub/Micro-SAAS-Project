23f0d5e46241e2d7830351d2fb6e14ca0415b806
rajubhai-hub <85005197+rajubhai-hub@users.noreply.github.com>
2026-03-16 16:13:05 +0530

    Set up Express server with a test route

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-Hi
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("DevOps CI/CD Pipeline with Jenkins and Docker is working!");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
